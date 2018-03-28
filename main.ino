50a808eb218f8823ca840fbd1702f85c372d2a11
Yamuyamuso <laurentwu123@gmail.com>
2018-03-28 16:23:57 +0200

    fini complet

--- main.ino
+++ main.ino
@@ -2,13 +2,46 @@
 //
 
 #include "Arduino.h"
+#include "my_header.hpp"
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
+    pinMode(RedPin, OUTPUT);
+    pinMode(GreenPin, OUTPUT);
+    pinMode(BluePin, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
+    static int potPosition;
+    potPosition = analogRead(A1);
+    if (potPosition > 0 && potPosition <= 150)
+    {
+        red();
+    }
+    if (potPosition > 150 && potPosition <= 300)
+    {
+        orange();
+    }
+    if (potPosition > 300 && potPosition <= 450)
+    {
+        yellow();
+    }
+    if (potPosition > 450 && potPosition <= 600)
+    {
+        green();
+    }
+    if (potPosition > 600 && potPosition <= 750)
+    {
+        cyan();
+    }
+    if (potPosition > 750 && potPosition <= 900)
+    {
+        blue();
+    }
+    if (potPosition > 900)
+    {
+    }
 }
